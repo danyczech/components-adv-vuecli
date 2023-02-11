a6c37a4e73293fdef9f53d146cccc2ecbad4fdbf
danakal <dana.kalcikova@centrum.cz>
2023-02-11 14:14:16 +0100

    dynamic and allert components

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,50 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></UserInfo>
+    <CourseGoals #default="slotProps">
+      <h2> {{ slotProps.item }}</h2>
+      <p>{{ slotProps['anotherprop'] }}</p>
+     
+      <!-- universal template, if it is default slot and the only one , can be shortened
+      <template #default="slotProps">
+        <h2> {{ slotProps.item }}</h2>
+        <p>{{ slotProps['anotherprop'] }}</p>
+      </template>
+      -->
+    </CourseGoals>
+
+    <button @click="setSelectedComponent('ManageGoals')">Manage Goals</button>
+    <button @click="setSelectedComponent('ActiveGoals')">Active Goals</button>
+    <ManageGoals></ManageGoals>
+    <ActiveGoals></ActiveGoals>
+
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+    
   </div>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue"
+import TheHeader from "./layout/TheHeader.vue"
 import BadgeList from "./components/BadgeList.vue"
 import UserInfo from "./components/UserInfo.vue"
+import CourseGoals from "./components/CourseGoals.vue"
+import ManageGoals from "./components/ManageGoals.vue"
+import ActiveGoals from "./components/ActiveGoals.vue"
 
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
+    ManageGoals,
+    ActiveGoals
   },
   data() {
     return {
+      selectedComponent: 'ActiveGoals',
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
@@ -30,6 +58,11 @@ export default {
       },
     }
   },
+  methods: {
+    setSelectedComponent (cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 }
 </script>
 
